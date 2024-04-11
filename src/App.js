58e23586ea922b0ffafb0876279288c8a77b44ce
Ravi Verma <vravi084@gmail.com>
2024-04-11 16:36:53 +0530

    Added functionality of adding data into databae

--- src/App.js
+++ src/App.js
@@ -1,50 +1,99 @@
-import React, { useState } from 'react'
-import './App.css'
-import List from './List'
+import React, { useState } from "react";
+import "./App.css";
+import List from "./List";
+import { app } from "./firebase";
+import { getDatabase, ref, set, push } from "firebase/database";
+
+const db = getDatabase(app);
+let noteId = 0;
 
 export default function App() {
-  
-  const [inputList,setInputList] = useState("");
-  const [items,setItems] = useState([]);
+  const [inputData, setinputData] = useState("");
+  const [items, setItems] = useState([]);
 
-  const itemEvent = (event)=>{
-    setInputList(event.target.value);
-  }
-  
-  const listOfItems = ()=>{
-    if(inputList.length > 0){
-      setItems((oldItems)=>{
-        return [...oldItems,inputList];
-      })
-      setInputList("");
-    }
-  }
-  
-  const deleteItems = (id)=>{
-    setItems((oldItems)=>{
-      return oldItems.filter((ele,idx)=>{
-        return idx !== id;
-      })
+  const itemEvent = (event) => {
+    setinputData(event.target.value);
+  };
+
+  const putDataIntoDatabase = () => {
+    set(ref(db, `Notes/Note ${noteId}`), {
+      id: noteId,
+      note: inputData,
     })
-  }
+      .then(() => {
+        console.log("Note added successfully !");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    
+
+    // Another method of adding data into database that will generate sub folder automatically
+    
+    // const postListRef = ref(db, 'Notes');
+    // const newPostRef = push(postListRef);
+    // set(newPostRef,{
+    //   id : noteId,
+    //   Note : inputData
+    // })
+
+    noteId++;
+  };
+
+  const listOfItems = () => {
+    if (inputData.length > 0) {
+      setItems((oldItems) => {
+        return [...oldItems, inputData];
+      });
+
+      putDataIntoDatabase();
+    }
+
+    setinputData("");
+  };
+
+  const deleteItems = (id) => {
+    setItems((oldItems) => {
+      return oldItems.filter((ele, idx) => {
+        return idx !== id;
+      });
+    });
+  };
+
   return (
     <>
-      <div className='mainDiv'>
-        <div className='centerDiv'>
-           <br/>
-           <h1>Note Taking App</h1>
-           <br/>
-           <input type='text' placeholder='Create New Note' 
-            onChange={itemEvent}  value={inputList}/>
-            
-           <button className='btn btn-secondary mx-2 rounded-pill' onClick={listOfItems}>➕</button>
-           <ol>
-             {items.map((itemValue,index)=>{
-              return <List itemVal={itemValue} key={index} id={index} onSelect={deleteItems}/>
-             })}
-           </ol>
+      <div className="mainDiv">
+        <div className="centerDiv">
+          <br />
+          <h1>Note Taking App</h1>
+          <br />
+          <input
+            type="text"
+            placeholder="Create New Note"
+            onChange={itemEvent}
+            value={inputData}
+          />
+
+          <button
+            className="btn btn-secondary mx-2 rounded-pill"
+            onClick={listOfItems}
+          >
+            ➕
+          </button>
+          <ol>
+            {items.map((itemValue, index) => {
+              return (
+                <List
+                  itemVal={itemValue}
+                  key={index}
+                  id={index}
+                  onSelect={deleteItems}
+                />
+              );
+            })}
+          </ol>
         </div>
       </div>
     </>
-  )
+  );
 }
